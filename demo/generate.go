35117193101ba731d356011d5fa21e6d69e7ce31
Gerard Moreno-Torres Bertran <gerardmtb@meta.com>
2026-01-31 21:37:08 +0000

    Remove unused demo/generate.go placeholder

--- demo/generate.go
+++
@@ -1,8 +0,0 @@
-// Demo CLI showing gox usage for building terminal UIs.
-//
-// This demonstrates how gox can be used to build component-based
-// terminal interfaces using a JSX-like syntax.
-//
-// Run:   gox run ./demo/
-// Build: gox build -o demo-app ./demo/
-package main
